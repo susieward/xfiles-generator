4ec20da89e0c202eb156861cf2beed030e1304fb
Susie Ward <susie@susieward.dev>
2021-01-27 21:04:46 -0600

    deployment related files/installs

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def index():
     return render_template('index.html', links = links, form = form, input = None)
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(threaded=True, port=5000)
