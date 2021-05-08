6759493d40360e0661a9ae5546d2f9c7afd7617d
Susie Ward <susie@susieward.dev>
2021-05-08 12:34:43 -0500

    made temp a dropdown, css edits, replaced readable stream with async generator

--- static/main.js
+++ static/main.js
@@ -2,69 +2,60 @@ const output = document.getElementById('output')
 const start_string = document.getElementById('start_string')
 const temp = document.getElementById('temperature')
 const char_length = document.getElementById('char_length')
+const submitButton = document.getElementById('submit-button')
 
-temp.addEventListener('input', validateInput)
 char_length.addEventListener('input', validateChars)
 
 async function getCharStream(){
   try {
-    if(start_string.value && temp.value && char_length.value){
-      const reqData = {
-        start_string: start_string.value,
-        temp: temp.value,
-        char_length: char_length.value,
-      }
-      const res = await fetch('/stream', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Connection': 'keep-alive'
-        },
-        body: JSON.stringify(reqData),
-      })
-      const reader = res.body.getReader();
-      const decoder = new TextDecoder('utf-8')
-      let result = `${start_string.value}`
-
-      const stream = await new ReadableStream({
-        start(controller) {
-          push();
-          function push() {
-            return reader.read().then(({ done, value }) => {
-              if(done){
-                controller.close()
-                return
-              }
-              const chunk = decoder.decode(value)
-              controller.enqueue(chunk)
-              result += chunk
-              output.innerHTML = result
-              push()
-            })
-          }
-        }
-      })
-      return new Response(stream);
+    if(!start_string.value || !temp.value || !char_length.value){
+      return output.innerHTML = 'Please fill in all input fields.'
     }
-    return output.innerHTML = 'Please fill in all input fields.'
+    submitButton.setAttribute('disabled', true)
+
+    const reqData = JSON.stringify({
+      start_string: start_string.value,
+      temp: temp.value,
+      char_length: char_length.value,
+    })
+    const res = await fetch('/stream', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Connection': 'keep-alive'
+      },
+      body: reqData
+    })
+
+    const decoder = new TextDecoder('utf-8')
+    const generator = streamGenerator(res.body)
+    output.innerHTML = `${start_string.value}`
+
+    for await (const chunk of generator) {
+      const result = decoder.decode(chunk)
+      output.innerHTML += result
+    }
+    submitButton.removeAttribute('disabled')
   } catch(err){
     console.log('getCharStream err', err)
     output.innerHTML = `Error: ${err.message}`
   }
 }
 
-function validateInput(e){
-  const val = e.target.value;
-  let numVal = Number(val)
-  if(isNaN(numVal)){
-    e.preventDefault()
-    e.target.value = 0.6
-    return
-  }
-  if(val.length > 4){
-    e.preventDefault()
-    e.target.value = val.substring(val.indexOf('0.'), 4)
-    return
+async function* streamGenerator(stream){
+  const reader = stream.getReader()
+  try {
+    while (true) {
+      const { done, value } = await reader.read()
+      if (done) {
+        return
+      }
+      yield value
+    }
+  } catch(err) {
+    throw err
+  } finally {
+    reader.releaseLock()
   }
 }
 
