3853667d8a4c3aab1e48f422ddc4a31fe5b59830
Susie Ward <susie@susieward.dev>
2021-10-16 10:52:34 -0500

    adding line breaks in comments for reasons

--- app/models/transformer/custom_model.py
+++ app/models/transformer/custom_model.py
@@ -1,4 +1,6 @@
-# "sample" method modified from original in GenerationMixin class (https://huggingface.co/transformers/_modules/transformers/generation_utils.html), copyright 2020 The Google AI Language Team Authors, Facebook AI Research authors and The HuggingFace Inc. team.
+# "sample" method modified from original in GenerationMixin class
+# (https://huggingface.co/transformers/_modules/transformers/generation_utils.html)
+# Copyright 2020 The Google AI Language Team Authors, Facebook AI Research authors and The HuggingFace Inc. team.
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
 
 from transformers import GPT2LMHeadModel
